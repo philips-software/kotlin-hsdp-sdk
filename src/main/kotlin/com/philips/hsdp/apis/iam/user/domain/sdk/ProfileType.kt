e07c9807e8e73a1046081c813bbd4cd8fc8c13cb
Aad Rijnberg <83588870+aadrijnberg@users.noreply.github.com>
2021-11-17 09:56:11 +0100

    Incorporate review comments
    - Add an optional profileType parameter to the search method

--- src/main/kotlin/com/philips/hsdp/apis/iam/user/domain/sdk/ProfileType.kt
+++ src/main/kotlin/com/philips/hsdp/apis/iam/user/domain/sdk/ProfileType.kt
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2020-2021, Koninklijke Philips N.V., https://www.philips.com
+ * SPDX-License-Identifier: MIT
+ */
 package com.philips.hsdp.apis.iam.user.domain.sdk
 
 enum class ProfileType(val value: String) {
